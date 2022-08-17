4f7bb22a8754e18a63fb218eb464fcb69344b016
dvxgit-tbautista <110645703+dvxgit-tbautista@users.noreply.github.com>
2022-08-16 20:56:09 -0700

    User login created

--- routes/api.php
+++ routes/api.php
@@ -20,7 +20,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::apiResource('posts', PostController::class);
+Route::apiResource('posts', PostController::class)->middleware('auth:sanctum');
 
 
 Route::post('/auth/register', [AuthController::class, 'createUser']);
